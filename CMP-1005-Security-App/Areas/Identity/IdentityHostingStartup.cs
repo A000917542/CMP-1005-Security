1c3190f845913181a6f1a24120504ae787611a2d
Brent Ritchie <ritchie.brent@gmail.com>
2022-03-14 21:21:13 -0400

    Added Identity Scaffolds

---
+++ CMP-1005-Security-App/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CMP_1005_Security_App.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CMP_1005_Security_App.Areas.Identity.IdentityHostingStartup))]
+namespace CMP_1005_Security_App.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            //builder.ConfigureServices((context, services) => {
+            //    services.AddDbContext<ApplicationDbContext>(options =>
+            //        options.UseSqlServer(
+            //            context.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
+            //    services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //        .AddEntityFrameworkStores<ApplicationDbContext>();
+            //});
+        }
+    }
+}
